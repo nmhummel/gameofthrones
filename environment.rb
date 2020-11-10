e55da13d9c62a621b8b31fe3439bd4e9d9f389f3
nmhummel <natalie.hummel@gmail.com>
2020-11-09 21:06:33 -0600

    still trying

--- environment.rb
+++ environment.rb
@@ -9,7 +9,7 @@ require 'open-uri'
 
 require_relative './lib/api.rb'
 require_relative './lib/cli.rb'
-#require_relative './lib/character.rb'
-#require_relative './lib/book.rb'
+require_relative './lib/character.rb'
+require_relative './lib/book.rb'
 require_relative './lib/house.rb'
 
