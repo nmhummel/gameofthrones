3109b9ee13b41648cabc202006cf854463c0a463
nmhummel <natalie.hummel@gmail.com>
2020-11-11 14:51:12 -0600

    program working 99%. there's one error that isn't consistent. working all possible paths. need to add options remaining so user doesn't reselect option and get an error

--- environment.rb
+++ environment.rb
@@ -3,13 +3,11 @@
 require 'pry'
 require 'json'
 require 'httparty'
-# require 'net/http'
 require 'open-uri'
 
 
 
 require_relative './lib/api.rb'
-require_relative './lib/region.rb'
 require_relative './lib/house.rb'
 require_relative './lib/cli.rb'
 
--- lib/cli.rb
+++ lib/cli.rb
@@ -12,8 +12,27 @@ class Cli
 
     # welcome, grab info, and take us to other method that controls main object
     def start
-        puts "A WELCOME GUIDE TO THE NORTH"
-        sleep(2)        
+        puts "A WELCOME GUIDE TO THE NORTH"   
+        sleep(1)    
+        puts "So there's a big hole in The Wall and you came through it. Now what?"
+        puts "======================================================================"
+        puts "---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+--"
+        puts "o | o   o | o   o | o   o | o   o | o   o | o   o | o   o | o   o | o"
+        puts "---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+--"
+        puts "o | o   o | o   o | o   o | o   o | o   o | o   o | o   o | o   o | o"
+        puts "---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+--"
+        puts "o   o | o   o | o   o | o   o | o   o | o   o | o   o | o   o | o   o"
+        puts "---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+--"
+        puts "o | o   o | o   o | o   o | o   o | o   o | o   o | o   o | o   o | o"
+        puts "---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+--"
+        puts "o   o | o   o | o   o | o   o | o   o | o   o | o   o | o   o | o   o"
+        puts "---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+--"
+        puts "o | o   o | o   o | o   o | o   o | o   o | o   o | o   o | o   o | o"
+        puts "---+---+---+---+---+---+---+---+---+---+---+---+---+             --+--"
+        puts "o   o | o   o | o   o | o   o | o   o | o   o | o               o   o"
+        puts "---+---+---+---+---+---+---+---+---+---+---+---+---+-            --+--"
+        puts "o | o   o | o   o | o   o | o   o | o   o | o   o |             o | o"
+        puts "---+---+---+---+---+---+---+---+---+---+---+---+---+-           ---+--"
         puts "\n" # new line
         puts "The Game of Thrones has ended, but that doesn't mean you can just march into The North without some allies."
         puts "You really need to become familiar with the different Houses around here. Good thing I'm here to help."
@@ -25,27 +44,27 @@ class Cli
         puts "\n" # new line
         puts "Would you like to meet some potential allies? (type yes or no)"
         user_input = gets.strip.downcase # user inputs y/n
-        if user_input == "no" 
-            if @@all.empty?
-                puts "Farewell, then. With no allies, I expect you'll be dead in less than a fortnight."
-            else 
-                puts "You have chosen the following houses as your allies:"
-                puts @@all
-                puts "Now go forth and introduce yourself. Good luck!"
-            end
-        else  
+        if user_input == "yes"  
+            puts "\n" # new line
             puts "Wise decision."
-            sleep(1)
             puts "\n" # new line
             puts "Let's take a look at some of the houses that are held in the highest regard."
             puts "Please choose by entering the number of the House you wish to explore:"
             puts "\n" # new line
             display_list_of_houses
             ask_user_for_house_choice
-            sleep(1)
             menu
+        elsif          
+            puts "Please select a valid choice."
+            menu
+        else 
+            user_input == "no"
+            exit_program
         end
     end
+
+    # def first_user_input
+    # end
 
     # .each is best for printing out
     def display_list_of_houses
@@ -53,7 +72,6 @@ class Cli
             puts "#{index}. #{house.name}"  # interpolate
         end
         puts "\n" # new line
-        puts ">"
     end
          
    def ask_user_for_house_choice
@@ -88,19 +106,71 @@ class Cli
     def save_chosen_house(house)
         puts "\n" # new line
         puts "Do you think you could be allies? (choose yes or no)"
-        input_yn = gets.strip
-        if input_yn == "yes"
-            @@all << house
+        puts "\n" # new line
+        input = gets.strip.downcase
+        if input == "yes"
+            @@all << house.name.to_s   
+            puts "\n" # new line
+            puts "Excellent. I have saved this choice for you in your journal."
+            puts "\n" # new line
         else puts "\n" # new line
             puts "Very well. Let's find you another House."
             puts "\n" # new line
             menu
         end
     end
+
  
-    def self.clear # was on house.rb
-        @@all.clear
+ 
+    def exit_program
+        if @@all.empty?
+            puts "\n" # new line
+            puts "Farewell, then. With no allies, I expect you'll be dead in less than a fortnight."
+            puts "\n" # new line
+            sleep(2)
+            puts "                               __   "
+            puts "                            .d$$b   "
+            puts "                         .' TO$;\   "
+            puts "                       /  : TP._;   "
+            puts "                     / _.;  :Tb|   "
+            puts "                    /   /   ;j$j   "
+            puts "                _.-^       d$$$$^   "
+            puts "               .' ..       d$$$$;   "
+            puts "              /  /P'      d$$$$P. - }   "
+            puts "             /   ^      .d$$$P'^^^^}   "
+            puts "           .'           `T$P^^^^^^^  :   "
+            puts "      ._.'      _.'                ;   "
+            puts "    `-.-^.-'-' ._.       _.-    .-   "
+            puts "  `.-^ _____  ._              .-   "
+            puts " -(.g$$$$$$$b.              .'   "
+            puts "    ^^T$$$P^)            .(:   "
+            puts "       _/  -^  /.^         /:/;   "
+            puts "      ._.'-'`-^  ^)/         /; / ; "
+            puts "   `-.- ^..--^   ^ /         /  ;   "
+            puts "  .-^ ..--^^        -^          :   "
+            puts " ..--^^--.-^         (\      .-(\   "
+            puts "   ..--^^              `-\(\/;`   "
+            puts "  _.                      :   "
+            puts "                         ;`-   "
+            puts "                        :\   "
+            puts "                       ;  bug   "
+            exit
+        else 
+            puts "You have chosen the following houses as your allies:"
+            sleep(1)
+            puts "\n" # new line
+            puts @@all
+            puts "\n" # new line
+            sleep(1)
+            puts "Now go forth and introduce yourself. Good luck!"
+            exit
+        end
+        return
     end
+
+    # def self.clear # was on house.rb
+    #     @@all.clear
+    # end
 
 
 end
